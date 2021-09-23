e6f36130aff640809ff8b4bb50930da5185532fe
Julio Canizalez <juliocanizalez@outlook.com>
2021-09-23 13:18:51 -0600

    :hammer: Add import from nextui css baseline

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CssBaseline } from '@nextui-org/react';
+
 import App from './views/App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
