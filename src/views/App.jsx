e00952afa048e2628e280d6140b4a47650750b20
Julio Canizalez <juliocanizalez@outlook.com>
2021-09-24 08:55:12 -0600

    :sparkles: Add content to app

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Grid, Text, Spacer, Avatar } from '@nextui-org/react';
+import { Grid, Text, Spacer, Avatar, Loading } from '@nextui-org/react';
+import { connect } from 'react-redux';
 
 import { machineImage } from '../utils/utils';
+import MachineItem from '../components/MachineItem';
 
-const App = () => {
+const App = ({ data }) => {
   return (
     <>
       <Spacer y={3} />
@@ -15,7 +17,27 @@ const App = () => {
         </Grid>
 
         <Grid xs={12} md={6} justify='center'>
-          <Text h3>List of products</Text>
+          <Grid.Container gap={1}>
+            <Grid xs={12} justify='center'>
+              <Text h3>List of products</Text>
+            </Grid>
+            {!data && (
+              <Grid xs={12} justify='center' alignItems='center'>
+                <Loading color='secondary'>Loading data...</Loading>
+              </Grid>
+            )}
+            {data &&
+              data.map((item, index) => (
+                <Grid xs={12} justify='center'>
+                  <MachineItem
+                    key={index}
+                    name={item.name}
+                    time={`${item.preparation_time}s`}
+                    image={item.thumbnail}
+                  />
+                </Grid>
+              ))}
+          </Grid.Container>
         </Grid>
         <Grid xs={12} md={6}>
           <Grid.Container gap={1}>
@@ -32,4 +54,10 @@ const App = () => {
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    data: state.data,
+  };
+};
+
+export default connect(mapStateToProps, null)(App);
