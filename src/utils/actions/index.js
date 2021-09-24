b7ae2f83244e366ec7e2a4c0fcb2a665172d32d2
Julio Canizalez <juliocanizalez@outlook.com>
2021-09-24 12:28:21 -0600

    :hammer: Add actions

---
+++ src/utils/actions/index.js
@@ -0,0 +1,14 @@
+export const setPrepare = (payload) => ({
+  type: 'SET_PREPARE',
+  payload,
+});
+
+export const setFood = (payload) => ({
+  type: 'SET_FOOD',
+  payload,
+});
+
+export const setDone = (payload) => ({
+  type: 'SET_DONE',
+  payload,
+});
