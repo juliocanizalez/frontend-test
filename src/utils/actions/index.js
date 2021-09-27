a2bc4498b040ddc9fe9f2b082c024e3c66b445a9
Julio Canizalez <juliocanizalez@outlook.com>
2021-09-26 23:46:22 -0600

    :recycle: Refactor action types

--- src/utils/actions/index.js
+++ src/utils/actions/index.js
@@ -1,14 +1,16 @@
+import { SET_PREPARE, SET_DONE, SET_FOOD } from './actionTypes';
+
 export const setPrepare = (payload) => ({
-  type: 'SET_PREPARE',
+  type: SET_PREPARE,
   payload,
 });
 
 export const setFood = (payload) => ({
-  type: 'SET_FOOD',
+  type: SET_FOOD,
   payload,
 });
 
 export const setDone = (payload) => ({
-  type: 'SET_DONE',
+  type: SET_DONE,
   payload,
 });
