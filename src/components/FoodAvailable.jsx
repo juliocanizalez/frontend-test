60ed9630b3c1f55a54a80dce35311028e8736b78
Julio Canizalez <juliocanizalez@outlook.com>
2021-09-27 12:20:21 -0600

    :sparkles: Update product display and connect

--- src/components/FoodAvailable.jsx
+++ src/components/FoodAvailable.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
 import { Grid, Loading } from '@nextui-org/react';
 import { connect } from 'react-redux';
+import { map } from 'lodash';
 
 import MachineItem from './MachineItem';
 
-const FoodAvailable = ({ data }) => {
-  return (
-    <>
-      {!data && (
-        <Grid xs={12} justify='center' alignItems='center'>
-          <Loading color='secondary'>Loading data...</Loading>
+const FoodAvailable = ({ data }) => (
+  <>
+    {data.loading ? (
+      <Grid xs={12} justify='center' alignItems='center'>
+        <Loading color='secondary'>Loading data...</Loading>
+      </Grid>
+    ) : (
+      map(data, (food) => (
+        <Grid key={food.id} xs={12} justify='center'>
+          <MachineItem name={food.name} time={`${food.preparation_time}s`} image={food.thumbnail} />
         </Grid>
-      )}
-      {data &&
-        data.map((item, index) => (
-          <Grid xs={12} justify='center'>
-            <MachineItem
-              key={index}
-              name={item.name}
-              time={`${item.preparation_time}s`}
-              image={item.thumbnail}
-            />
-          </Grid>
-        ))}
-    </>
-  );
-};
+      ))
+    )}
+  </>
+);
 
 const mapStateToProps = (state) => ({
   data: state.data,
