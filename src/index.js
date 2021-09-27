5d21d6d24e0a121383498c4be2dd061f92db9f60
Julio Canizalez <juliocanizalez@outlook.com>
2021-09-27 00:35:09 -0600

    :hammer: Add redux utils and config

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CssBaseline } from '@nextui-org/react';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './views/App';
-import { Store } from './utils/store';
+import reducer from './utils/reducers';
+import { setFood } from './utils/actions';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+store.dispatch(setFood());
 
 ReactDOM.render(
-  <Provider store={Store}>
+  <Provider store={store}>
     <CssBaseline />
     <App />
   </Provider>,
