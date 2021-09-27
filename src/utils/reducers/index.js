e5a20ad8f06bcf8762510b701621f6a2a1578123
Julio Canizalez <juliocanizalez@outlook.com>
2021-09-27 00:55:30 -0600

    :hammer: Update reducers behavior

--- src/utils/reducers/index.js
+++ src/utils/reducers/index.js
@@ -1,5 +1,29 @@
-const reducer = (state, action) => {
-  return state;
+import { keyBy } from 'lodash';
+
+import { SET_FOOD, SET_FOOD_DONE } from '../actions/actionTypes';
+
+const initialState = {
+  data: {},
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_FOOD:
+      return {
+        ...state,
+      };
+    case SET_FOOD_DONE:
+      return {
+        ...state,
+        data: keyBy(
+          action.payload.data.map((food) => ({
+            ...food,
+          })),
+          'id'
+        ),
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
