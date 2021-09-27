499cb30c8cf52c3b4b62e2a32e4ead221d373b91
Julio Canizalez <juliocanizalez@outlook.com>
2021-09-27 12:54:01 -0600

    :recycle: Refactor reducers

--- src/utils/reducers/index.js
+++ src/utils/reducers/index.js
@@ -3,7 +3,7 @@ import { keyBy } from 'lodash';
 import { SET_FOOD, SET_FOOD_DONE } from '../actions/actionTypes';
 
 const initialState = {
-  loading: false,
+  loading: true,
   data: {},
 };
 const reducer = (state = initialState, action) => {
@@ -11,16 +11,13 @@ const reducer = (state = initialState, action) => {
     case SET_FOOD:
       return {
         ...state,
-        loading: true,
       };
     case SET_FOOD_DONE:
       return {
-        ...state,
         loading: false,
         data: keyBy(
           action.payload.data.data.map((food) => ({
             ...food,
-            loading: false,
           })),
           'id'
         ),
