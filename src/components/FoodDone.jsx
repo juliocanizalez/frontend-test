8b09d0d9c042159b1807dd2a7b25f57978fc8b2f
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-04 02:53:07 -0600

    :hammer: Update layout

--- src/components/FoodDone.jsx
+++ src/components/FoodDone.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, Grid } from '@nextui-org/react';
+import { Button, Grid, Spacer } from '@nextui-org/react';
 import { map } from 'lodash';
 
 import { setClear } from '../utils/actions';
 import DoneItem from './DoneItem';
+import Container from './Container/Container';
 
 const FoodDone = ({ done, clearList }) => {
   return (
-    <Grid.Container justify='center' gap={1} alignItems='center'>
-      {map(done, (food, index) => (
-        <Grid key={index} xs={12} justify='center' alignItems='center'>
-          <DoneItem name={food.name} image={food.image} />
-        </Grid>
-      ))}
-      <Button onClick={clearList} color='gradient' auto>
-        Clear List
-      </Button>
-    </Grid.Container>
+    <>
+      <Grid.Container justify='center' gap={1} alignItems='center'>
+        <Container side='right'>
+          {map(done, (food, index) => (
+            <Grid key={index} xs={12} justify='center' alignItems='center'>
+              <DoneItem name={food.name} image={food.image} />
+            </Grid>
+          ))}
+        </Container>
+        <Spacer y={3} />
+        <Button onClick={clearList} color='gradient' auto>
+          Clear List
+        </Button>
+      </Grid.Container>
+    </>
   );
 };
 
