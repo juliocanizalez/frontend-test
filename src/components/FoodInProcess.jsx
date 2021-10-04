114ab346606d23f04560d5798e1f4e565994d916
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-04 01:58:34 -0600

    :sparkles: Add Food in process component

---
+++ src/components/FoodInProcess.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Grid } from '@nextui-org/react';
+import { map } from 'lodash';
+
+import { setDone } from '../utils/actions';
+import ProcessItem from '../components/ProcessItem';
+
+const FoodInProcess = ({ prepare, onProductDone }) => {
+  return (
+    <Grid.Container justify='center' gap={1} alignItems='center'>
+      {map(prepare, (foodInProcess, index) => (
+        <Grid key={index} xs={12} justify='center' alignItems='center'>
+          <ProcessItem
+            name={foodInProcess.name}
+            image={foodInProcess.image}
+            secs={foodInProcess.time}
+            handleProduct={onProductDone}
+          />
+        </Grid>
+      ))}
+    </Grid.Container>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  prepare: state.prepare,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onProductDone: (food) => dispatch(setDone(food)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(FoodInProcess);
